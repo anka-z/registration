2f7210d9d3a33970b26b59b47b9e9e56bcae4b54
unknown <Anka>
2024-07-01 18:52:46 +0200

    added Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => (
+    <nav>
+      <h1>Rejestracja na wydarzenia</h1>
+    </nav>
+  );
+  
+  export default Navbar;
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,10 @@
 import ListTables from "@/components/list-tables";
+import Navbar from "@/components/Navbar";
 
 export default function App() {
   return (
     <div className="container m-4">
+      <Navbar />
       <h1>Test</h1>
       <h2>What tables do you have in your database?</h2>
       <ListTables />
