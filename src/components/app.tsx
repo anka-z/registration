59add56347818fd291b64f8b1851d32b5ec0f75c
anka-z <judd01@gmail.com>
2024-07-01 11:09:05 +0200

    text changed

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="container m-4">
       <h1>Test</h1>
-      <h2>What tables do you have in your db?</h2>
+      <h2>What tables do you have in your database?</h2>
       <ListTables />
     </div>
   );
