6b46c021a103152e3b47bcddf010d80149b29e93
unknown <Anka>
2024-07-02 17:17:25 +0200

    updated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">MNN</a>
+    <a className="navbar-brand" href="/">MNN</a>
     <div className="collapse navbar-collapse">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
