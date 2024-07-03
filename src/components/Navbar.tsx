6262d955b0953b439272572bd74d4f3ad5d2e522
unknown <Anka>
2024-07-03 19:14:54 +0200

    navbar updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,61 @@
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import React, { useState } from "react";
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-    <a className="navbar-brand" href="/">MNN</a>
-    <div className="collapse navbar-collapse">
-      <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
-          <Link className="nav-link" href="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/events">Wydarzenia</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/register">Rejestracja</Link>
-        </li>
-        <li className="nav-item">
-            <Link className="nav-link" href="/admin">Admin</Link>
-          </li>
-      </ul>
-    </div>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          MNN
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleCollapse}
+          aria-controls="navbarNav"
+          aria-expanded={!isCollapsed}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${isCollapsed ? "" : "show"}`}
+          id="navbarNav"
+        >
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" href="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/events">
+                Wydarzenia
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/register">
+                Rejestracja
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/admin">
+                Admin
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
